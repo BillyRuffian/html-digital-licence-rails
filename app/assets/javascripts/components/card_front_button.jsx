1ada7d37dbffbee68c207ca44792848eafe28655
Nigel Brookes-Thomas <nigel.brookes-thomas@digital.dvla.gov.uk>
2016-05-17 10:56:30 +0100

    added a 'back' button

---
+++ app/assets/javascripts/components/card_front_button.jsx
@@ -0,0 +1,21 @@
+var CardFrontButton = React.createClass({
+
+	render: function() {
+		return(
+			<div className="component-wrapper">
+				<div className="list-block">
+					<ul>
+						<li>
+							<a href="/cards" id="card-front-button" className="item-link list-button">Back to Licence</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		);
+	}
+});
+
+// ReactDOM.render(
+//  <Controls />,
+//  document.getElementById('controls')
+// );
